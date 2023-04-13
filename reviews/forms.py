25001181a762d0b5ed5bb1bf93bc5baa36e47a4b
pty9714 <pty9714@naver.com>
2023-04-13 16:30:39 +0900

    댓글 문제발생

--- reviews/forms.py
+++ reviews/forms.py
@@ -7,6 +7,13 @@ class ReviewForm(forms.ModelForm):
         exclude = ["user"]
 
 class CommentForm(forms.ModelForm):
+    # content = forms.CharField(
+    #     widget=forms.TextInput()
+    # )
+   
+    
     class Meta:
         model = Comment
         fields = ('content',)
+
+    
