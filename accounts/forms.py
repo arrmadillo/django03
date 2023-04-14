c09b30648e2843a107533c5c35c982855d3fc550
pty9714 <pty9714@naver.com>
2023-04-14 14:44:57 +0900

    프로필 페이지

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,10 +1,13 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
 
 class CustomUserCreationForm(UserCreationForm):
+    image = forms.ImageField(required=False)
+
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
-        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2',)
+        fields = ('username', 'email', 'first_name', 'last_name', 'image', 'password1', 'password2',)
 
 
 class CustomUserChangeForm(UserChangeForm):
